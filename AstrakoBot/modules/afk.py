21e65dd2796a7c3ffd863763eeb829dc5ce98f49
zeki039 <81940045+zeki039@users.noreply.github.com>
2021-04-24 08:09:34 +0700

    Update afk.py

--- AstrakoBot/modules/afk.py
+++ AstrakoBot/modules/afk.py
@@ -36,7 +36,7 @@ def afk(update: Update, context: CallbackContext):
     sql.set_afk(update.effective_user.id, reason)
     fname = update.effective_user.first_name
     try:
-        update.effective_message.reply_text("{} ngentot dulu gaesss!{}".format(
+        update.effective_message.reply_text("{} pasti emaknya teriak hape terosss{}".format(
             fname, notice))
     except BadRequest:
         pass
@@ -57,9 +57,7 @@ def no_longer_afk(update: Update, context: CallbackContext):
         firstname = update.effective_user.first_name
         try:
             options = [
-                '{} wah pasti abis ngentot!', '{} is back!', '{} ngentot terus gblk!',
-                '{} bagi pap tt om!', '{} si anjing balik lagi!', '{} si goblok sok sibuk!',
-                'Welcome back anak kampang! {}', 'kemana si {}?\nPasti abis parming pap tt!'
+                '{} welcome back babu', '{} is back!', '{} beban ortu balik lagi nih',
             ]
             chosen_option = random.choice(options)
             update.effective_message.reply_text(chosen_option.format(firstname))
