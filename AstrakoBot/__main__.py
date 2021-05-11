570252477139e5abac3f9424a68ac975f7129680
zeki039 <81940045+zeki039@users.noreply.github.com>
2021-05-11 12:08:45 +0700

    Update __main__.py

--- AstrakoBot/__main__.py
+++ AstrakoBot/__main__.py
@@ -224,7 +224,7 @@ def start(update: Update, context: CallbackContext):
                         [
                             InlineKeyboardButton(
                                 text="Help & Command",
-                                url="t.me/ranndompeople_bot?start=help",
+                                url="t.me/{dispatcher.bot.username}?start=help",
                             )
                         ],
                         [
